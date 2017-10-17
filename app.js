76d9ebdf7a324ed58bd0fc720ecda3f7e811128e
xinyunkang <xinyunkang@gmail.com>
2017-10-17 12:22:11 -0500

    set up Gulp

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+
+var app = express();
+
+var port = process.env.PORT || 5000;
+
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
+
+app.get('/', function (req, res) {
+    res.send("Hello");
+})
+
+app.get('/books', function (req, res) {
+    res.send("Hello books");
+})
+
+app.listen(5000, function (err) {
+    console.log('running server on port ' + port);
+})
