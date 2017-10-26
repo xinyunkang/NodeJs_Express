1092b8415a0ca021ac031cb83becf25fb15e69da
xinyunkang <xinyunkang@gmail.com>
2017-10-25 22:52:06 -0500

    template engine  EJS

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ var app = express();
 var port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
-app.use(express.static('src/views'));
+app.set('views','./src/views');
+app.set('view engine', 'ejs');
 
 
 app.get('/', function (req, res) {
-    res.send("Hello");
-})
+    res.render('index', {title: 'Hello from render',list: ['a','b']});
+});
 
 app.get('/books', function (req, res) {
     res.send("Hello books");
