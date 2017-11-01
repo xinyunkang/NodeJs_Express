88b22baf9f348f100e827a851ed0e442b2f105b8
xinyunkang <xinyunkang@gmail.com>
2017-11-01 10:58:27 -0500

    Authentication  Passport

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var passport = require('passport');
+var session = require('express-session');
 
 var app = express();
 
@@ -6,21 +10,34 @@ var port = process.env.PORT || 5000;
 var nav = [{
     Link: '/Books',
     Text: 'Book'
-    }, {
+}, {
     Link: '/Authors',
     Text: 'Author'
-    }];
+}];
 var bookRouter = require('./src/routes/bookRoutes')(nav);
 var adminRouter = require('./src/routes/adminRoutes')(nav);
+var authRouter = require('./src/routes/authRoutes')(nav);
+
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+//app.use(session({secret: 'library'}));
+app.use(session({
+    secret: 'library',
+    resave: true,
+    saveUninitialized: true
+}));
+
+require('./src/config/passport')(app);
+
 app.set('views', './src/views');
 
 app.set('view engine', 'ejs');
 
-
 app.use('/Books', bookRouter);
 app.use('/Admin', adminRouter);
-
+app.use('/Auth', authRouter);
 
 app.get('/', function (req, res) {
     res.render('index', {
--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,47 +1,63 @@
 var express = require('express');
-
 var bookRouter = express.Router();
 var mongodb = require('mongodb').MongoClient;
 var objectId = require('mongodb').ObjectID;
 
-var router = function(nav){
+var router = function (nav) {
 
-    bookRouter.route('/')
-    .get(function (req, res) {
-        
-        var url = 'mongodb://localhost:27017/libraryApp';
-        mongodb.connect(url, function(err,db){
-            var collection= db.collection('books');
-            collection.find({}).toArray(
-                function(err, results){
-                    res.render('bookListView', {
-                        title: 'Books',
-                        nav: nav,
-                        books: results
-                    });
-                });
-            }            
-        )
+    bookRouter.use(function (req, res, next) {
+        if (!req.user) {
+            res.redirect('/');
+        }
+        next();
     });
+    bookRouter.route('/')
+        .get(function (req, res) {
+            var url =
+                'mongodb://localhost:27017/libraryApp';
+
+            mongodb.connect(url, function (err, db) {
+                var collection = db.collection('books');
+
+                collection.find({}).toArray(
+                    function (err, results) {
+                        res.render('bookListView', {
+                            title: 'Books',
+                            nav: nav,
+                            books: results
+                        });
+                    }
+                );
+            });
+
+        });
 
     bookRouter.route('/:id')
-    .get(function (req, res) {
-        var id = new objectId(req.params.id);
-         var url = 'mongodb://localhost:27017/libraryApp';
-        mongodb.connect(url, function(err,db){
-            var collection= db.collection('books');
-            collection.findOne({_id: id},
-                function(err, results){
-                    res.render('bookView', {
-                        title: 'Books',
-                        nav: nav,
-                        book: results
-                    });
-                });
-            }            
-        )
-    });
-    
+        .get(function (req, res) {
+            var id = new objectId(req.params.id);
+            var url =
+                'mongodb://localhost:27017/libraryApp';
+
+            mongodb.connect(url, function (err, db) {
+                var collection = db.collection('books');
+
+                collection.findOne({
+                        _id: id
+                    },
+                    function (err, results) {
+                        res.render('bookView', {
+                            title: 'Books',
+                            nav: nav,
+                            book: results
+                        });
+
+                    }
+                );
+
+            });
+
+        });
+
     return bookRouter;
-}
+};
 module.exports = router;
